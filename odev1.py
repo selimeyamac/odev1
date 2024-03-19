43839a46496278939497b0f456fe251ebe0077bb
selimeyamac <164074136+selimeyamac@users.noreply.github.com>
2024-03-19 22:32:22 +0300

    odev1

---
+++ odev1.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+4-Dairenin alanını ve çevresini hesaplayan python kodunu yazınız.(Dairenin yarıçapını kullanıcıdan alınız)
+
+yaricap = input("Lutfen dairenin yaricapini giriniz:" )
+
+dairenincevresi = 2*3.14* yaricap
+daireninalani= 3.14*yaricap*yaricap
+print("Dairenin cevresi: " + str(dairenincevresi))
+print("Dairenin alani: " + str(daireninalani))
+
+
+# # #1-Kullanıcının girdiği boy ve ağırlık değerlerine göre vücut kitle indeksini (VKİ = ağırlık/(boy*boy)) hesaplayınız.,
+
+# # boy= input("Lutfen boyunuzu giriniz: ")
+# # kilo= input("Lutfen kilonuzu giriniz: ")
+
+# # vci= kilo/(boy*boy)
+# # print("Vucut kitle indeksiniz: "+str(vci))
+# #ctrl k c ctrl k u
+
+# #2-Maaşı ve zam oranı girilen işçinin zamlı maaşını hesaplayarak ekranda gösteriniz.
+# # maas=float(input("maas: "))
+# # zam=float(input("zam: "))
+
+# # yenimaas= maas + (maas*(zam/100))
+# # print("maasiniz "+str(yenimaas))
+
+# #3-Kullanıcının girdiği üç sayı arasında en büyük olanı bulan ve sonucu yazdıran bir program yazınız.
+
+# sayi1 =float(input("sayi giriniz: "))
+# sayi2 =float(input("sayi giriniz: "))
+# sayi3 =float(input("sayi giriniz: "))
+
+# if sayi1 >= sayi2 and sayi1 >= sayi3:
+#     en_buyuk = sayi1
+# elif sayi2 >= sayi1 and sayi2 >= sayi3:
+#     en_buyuk = sayi2
+# else:
+#     en_buyuk = sayi3
+# print(en_buyuk)
+
+# #5-Kullanıcıdan alınan bir sayının palindrom olup olmadığını bulan bir program yazınız.
+
+#selime
+
