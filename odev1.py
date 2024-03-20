2d1bf33d62bf04c758d70864c982c82a25e2b89c
selimeyamac <164074136+selimeyamac@users.noreply.github.com>
2024-03-20 22:07:28 +0300

    Update odev1.py
    güncellendi

--- odev1.py
+++ odev1.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-4-Dairenin alanını ve çevresini hesaplayan python kodunu yazınız.(Dairenin yarıçapını kullanıcıdan alınız)
+#4-Dairenin alanını ve çevresini hesaplayan python kodunu yazınız.(Dairenin yarıçapını kullanıcıdan alınız)
 
 yaricap = input("Lutfen dairenin yaricapini giriniz:" )
 
@@ -12,36 +12,59 @@ print("Dairenin alani: " + str(daireninalani))
 
 #1-Kullanıcının girdiği boy ve ağırlık değerlerine göre vücut kitle indeksini (VKİ = ağırlık/(boy*boy)) hesaplayınız.,
 
-# # boy= input("Lutfen boyunuzu giriniz: ")
-# # kilo= input("Lutfen kilonuzu giriniz: ")
+boy = int(input("Boyunuzu giriniz (cm): "))
+kilo = float(input("Kilonuzu giriniz: "))
+vki = kilo / ((boy / 100) ** 2)
+print("Vücut kitle index'iniz: {}".format(round(vki, 2)))
+print("Durumunuz: ")
+if vki <= 18.5:
+    print("Zayıf")
+    print("{} kilogram almaniz gerekiyor".format(round(18.5 * ((boy / 100) ** 2) - kilo, 2)))
+elif vki <= 24.9:
+    print("Normal")
+elif vki <= 29.9:
+    print("Fazla kilolu")
+    print("{} kilogram vermeniz gerekiyor".format(round(kilo - 24.9 * ((boy / 100) ** 2), 2)))
+elif vki <= 39.9:
+    print("Obez")
+    print("{} kilogram vermeniz gerekiyor".format(round(kilo - 24.9 * ((boy / 100) ** 2), 2)))
+else:
+    print("Aşırı obez")
+    print("{} kilogram vermeniz gerekiyor".format(round(kilo - 24.9 * ((boy / 100) ** 2), 2)))
 
-# # vci= kilo/(boy*boy)
-# # print("Vucut kitle indeksiniz: "+str(vci))
-# #ctrl k c ctrl k u
 
-# #2-Maaşı ve zam oranı girilen işçinin zamlı maaşını hesaplayarak ekranda gösteriniz.
-# # maas=float(input("maas: "))
-# # zam=float(input("zam: "))
+#2-Maaşı ve zam oranı girilen işçinin zamlı maaşını hesaplayarak ekranda gösteriniz.
 
-# # yenimaas= maas + (maas*(zam/100))
-# # print("maasiniz "+str(yenimaas))
+maas = int(input("Maaşinizi Girin:"))
+zamorani = int(input("Zam oranini girin:"))
+guncelmaas = maas + (maas * zamorani) / 100
+print(guncelmaas)
 
 # #3-Kullanıcının girdiği üç sayı arasında en büyük olanı bulan ve sonucu yazdıran bir program yazınız.
 
-sayi1 =float(input("sayi giriniz: "))
-sayi2 =float(input("sayi giriniz: "))
-sayi3 =float(input("sayi giriniz: "))
+a = int(input("a sayisi : " ))
+b = int(input("b sayisi : " ))
+c = int(input("c sayisi : " ))
 
-if sayi1 >= sayi2 and sayi1 >= sayi3:
-    en_buyuk = sayi1
-elif sayi2 >= sayi1 and sayi2 >= sayi3:
-    en_buyuk = sayi2
+if a>b and a>c:
+    print("a en büyük sayıdır")
+elif b>a and b>c:
+    print("b en büyük sayıdır")
 else:
-    en_buyuk = sayi3
-print(en_buyuk)
+    print("c en büyük sayıdır")
 
-# #5-Kullanıcıdan alınan bir sayının palindrom olup olmadığını bulan bir program yazınız.
+#5-Kullanıcıdan alınan bir sayının palindrom olup olmadığını bulan bir program yazınız.
 
-#selime
+
+metin = input('Metni Girin : \n')
+ters=metin[::-1]
+
+print('Girilen metnin tersi = %s' % (ters))
+if ters == metin:
+    print('Girilen metin palindrom')
+else:
+    print('Girilen metin palindrom değil.')
+
+
 
 
